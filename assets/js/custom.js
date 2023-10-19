904310ccfe5ec327b822630c784022ad856b94a0
SpikE <usamaahmed212aaa@gmail.com>
2023-10-19 18:23:40 +0600

    Modified assets/js/svg-loader.js | & and & | assets/js/custom.js jQuery Code to Javascript Es5 Code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,165 +1,198 @@
 "use strict";
-
-( function( $ ) {
-    window.addEventListener('load', function (event) {
-        // Preloader Js
-        var loader = document.querySelector(".loader");
-        if(loader != null) {
-            if (loader.classList !== "loader-hidden") {
-                loader.classList.add('loader-hidden');
-                enableScroll();
-                loader.addEventListener('transitionend', function(e) {
-                    loader.classList.add('loader-disable');
-                }, {
+window.addEventListener('load', function (event) {
+    // Preloader Js
+    var loader = document.querySelector(".loader");
+    if (loader != null) {
+        if (loader.classList !== "loader-hidden") {
+            loader.classList.add('loader-hidden');
+            enableScroll();
+            loader.addEventListener('transitionend', function (e) {
+                loader.classList.add('loader-disable');
+            }, {
                 capture: false,
                 once: true,
                 passive: false
-                });
-            }
-        }
-        
-        // Debounce Handler
-        function debounce(fn, delay = 300, immediate = true) {
-            var timeout;
-            return function executedFunction(e) {
-                e.stopPropagation();
-                e.preventDefault();
-                
-                var context = this;
-                var args = arguments;
-                var callNow = immediate && !timeout;
-
-                clearTimeout(timeout);
-
-                timeout = setTimeout(function() {
-                    timeout = null;
-                    if (!immediate) fn.apply(context, args);
-                        
-                }, delay);
-
-                if (callNow) fn.apply(context, args);
-            };
-        }
-
-        //insertAdjacentHTML Before all Images has data-src Attribute 
-        var inner_element = document.querySelector('.stage');
-        if(inner_element != null) {
-            inner_element = inner_element.outerHTML;
-        }
-        const images = document.querySelectorAll("img[data-src]");
-
-        if(images != null) {
-            images.forEach(function (element) {
-                if(inner_element != null) {
-                    element.insertAdjacentHTML('beforebegin', inner_element);
-                    element.previousElementSibling.removeAttribute("style");
-                }
             });
         }
+    }
 
-        // Print Current Year in a Website
-        const copyright_wrapper = document.querySelector("#copyright .copyright_wrapper");
-        if( copyright_wrapper != null ) {
-            copyright_wrapper.appendChild(document.createTextNode(new Date().getFullYear()));
-        }
+    // Debounce Handler
+    function debounce(fn, delay, immediate) {
+        var timeout;
+        return function executedFunction(e) {
+            e.stopPropagation();
+            e.preventDefault();
 
-        // .sidebar-menu Toggle  
-        if ( $("#menu-toggle").length > 0 ) { 
-            $("#menu-toggle").click(debounce(function(e) {
-                $(this).toggleClass('open');
-                if( $(".sidebar-menu").length > 0 ) {
-                    $(".sidebar-menu").toggleClass("active");
-                }
-            }));
-        }
+            var context = this;
+            var args = arguments;
+            var callNow = immediate && !timeout;
 
-        // .sidebar-menu Close  
-        if ( $(".sidebar-menu-close").length > 0 ) { 
-            $(".sidebar-menu-close").click(debounce(function(e) {
-                    $("#menu-toggle").removeClass('open');
-                    $(".sidebar-menu.active").removeClass("active");
-            }));
-        }
+            clearTimeout(timeout);
 
-        // Close or Hide .sidebar-menu when Click Outside 
-        $(document).on('click', function (e) {
-            var container = $(".sidebar-menu");
-            // If the target of the click isn't the container
-            if(!container.is(e.target) && container.has(e.target).length === 0){
-                $("#menu-toggle").removeClass('open');
-                $(".sidebar-menu.active").removeClass("active");
+            timeout = setTimeout(function () {
+                timeout = null;
+                if (!immediate) fn.apply(context, args);
+            }, delay);
+
+            if (callNow) fn.apply(context, args);
+        };
+    }
+
+    //insertAdjacentHTML Before all Images has data-src Attribute 
+    var innert_element = document.querySelector('.stage');
+    if (innert_element != null) {
+        innert_element = innert_element.outerHTML;
+    }
+    var images = document.querySelectorAll("img[data-src]");
+
+    if (images != null) {
+        images.forEach(function (element) {
+            if (innert_element != null) {
+                element.insertAdjacentHTML('beforebegin', innert_element);
+                element.previousElementSibling.removeAttribute("style");
             }
         });
+    }
 
-        // beforebegin Method add new Element the HTML into the container before the element
-        var inner_value = `<span class="sub-toggle">
-                        <svg width="14" height="14" viewBox="0 0 8 16">
-                            <path d="M0.772126 1.19065L0.153407 1.80934C0.00696973 1.95578 0.00696973 2.19322 0.153407 2.33969L5.80025 8L0.153407 13.6603C0.00696973 13.8067 0.00696973 14.0442 0.153407 14.1907L0.772126 14.8094C0.918563 14.9558 1.156 14.9558 1.30247 14.8094L7.84666 8.26519C7.99309 8.11875 7.99309 7.88131 7.84666 7.73484L1.30247 1.19065C1.156 1.04419 0.918563 1.04419 0.772126 1.19065Z"></path>
-                        </svg>
-                    </span>`;
+    // Print Current Year in a Website
+    var copyright = document.querySelector("#copyright");
+    if (copyright != null) {
+        copyright.appendChild(document.createTextNode(new Date().getFullYear()));
+    }
 
-        if ( $(".sub-menu").length > 0 ) { 
-            $(".sub-menu").each( function( index, value ) {
-                value.insertAdjacentHTML('beforebegin', inner_value);
-            });
+    // .sidebar-menu Toggle  
+    var menu_toggle = document.querySelector("#menu-toggle");
+    var sidebar_menu = document.querySelector(".sidebar-menu");
+
+    if (menu_toggle != null) {
+        menu_toggle.addEventListener('click', debounce(function (e) {
+            this.classList.toggle("open");
+            if (sidebar_menu != null) {
+                sidebar_menu.classList.toggle("active");
+            }
+        }));
+    }
+
+    // .sidebar-menu Close  
+    var sidebar_menu_close = document.querySelector(".sidebar-menu-close");
+
+    if (sidebar_menu_close != null) {
+        sidebar_menu_close.addEventListener('click', debounce(function (e) {
+            var sidebar_menu_active = document.querySelector(".sidebar-menu.active");
+
+            menu_toggle.classList.remove("open");
+            sidebar_menu_active.classList.remove("active");
+        }));
+    }
+
+    // Close or Hide .sidebar-menu when Click Outside 
+    window.addEventListener('click', function (e) {
+        if (menu_toggle.classList.contains("open") && sidebar_menu.classList.contains("active")) {
+            if (sidebar_menu !== e.target && !sidebar_menu.contains(e.target)) {
+                menu_toggle.classList.remove('open');
+                var sidebar_menu_active = document.querySelector(".sidebar-menu.active");
+                sidebar_menu_active.classList.remove("active");
+            }
         }
+    });
 
-        // .sidebar-menu > .sub-menu > .sub-toggle |&and&|.sidebar-menu > .sub-menu.active Toggle 
-        if ( $(".sub-toggle").length > 0 ) {
-            $(".sub-toggle").click(debounce(function(e) {
-                $(this).toggleClass('open');
+    // beforebegin Method add new Element the HTML into the container before the element |& and &| Preview Element Class Add 
+    var inner_value = '<span class="sub-toggle">' +
+        '<svg width="14" height="14" viewBox="0 0 8 16">' +
+        '<path d="M0.772126 1.19065L0.153407 1.80934C0.00696973 1.95578 0.00696973 2.19322 0.153407 2.33969L5.80025 8L0.153407 13.6603C0.00696973 13.8067 0.00696973 14.0442 0.153407 14.1907L0.772126 14.8094C0.918563 14.9558 1.156 14.9558 1.30247 14.8094L7.84666 8.26519C7.99309 8.11875 7.99309 7.88131 7.84666 7.73484L1.30247 1.19065C1.156 1.04419 0.918563 1.04419 0.772126 1.19065Z"></path>' +
+        '</svg>' +
+        '</span>';
 
-                var sub_menu = $(this).next();
-                if (sub_menu.height() == "0px" || sub_menu.height() == "") {
-                    sub_menu.animate({
-                        height: sub_menu.get(0).scrollHeight
-                }, 500, function(){
-                        $(this).height('auto');
-                });
+    var sub_menu = document.querySelectorAll('.sub-menu');
+    if (sub_menu != null) {
+        sub_menu.forEach(function (element) {
+            element.previousElementSibling.classList.add("dropdown-toggle");
+            element.insertAdjacentHTML('beforebegin', inner_value);
+        });
+    }
+
+    // .sidebar-menu > .sub-toggle Class Toggle |& and &| .sidebar-menu > .sub-menu
+    var sub_toggle_all = document.querySelectorAll(".sub-toggle");
+
+    if (sub_toggle_all != null) {
+        sub_toggle_all.forEach(function (element) {
+            element.addEventListener('click', debounce(function (e) {
+                this.classList.toggle("open");
+
+                var next_sub_menu = element.nextElementSibling;
+
+                if (next_sub_menu.offsetHeight == "0px" || next_sub_menu.offsetHeight == "") {
+                    var id = null;
+                    clearInterval(id);
+                    var height = 0;
+
+                    id = setInterval(function () {
+                        if (height >= next_sub_menu.scrollHeight) {
+                            next_sub_menu.style.height = "auto";
+                            clearInterval(id);
+                        } else {
+                            height += 5;
+                            next_sub_menu.style.height = height + "px";
+                        }
+                    }, 5);
                 } else {
-                    sub_menu.animate({
-                    height: "0"
-                }, 500);
+                    var id = null;
+                    clearInterval(id);
+                    var height = next_sub_menu.scrollHeight;
+
+                    id = setInterval(function () {
+                        if (height <= 0) {
+                            next_sub_menu.style.height = "0px";
+                            clearInterval(id);
+                        } else {
+                            height -= 5;
+                            next_sub_menu.style.height = height + "px";
+                        }
+                    }, 5);
                 }
             }));
-        }
+        });
+    }
 
-        // Images Lazy loading
-        function loadImage(e) {
-            var elements = document.querySelectorAll("img[data-src]");
-            elements.forEach(function (element) {
-                element.style.visibility = "hidden";
-                element.style.opacity = "0";
-                
-                if(element.hasAttribute("data-src") && element.getAttribute("data-src")) {
+    // Images Lazy loading
+    function loadImage(e) {
+        var elements = document.querySelectorAll("img[data-src]");
+        elements.forEach(function (element) {
+            element.style.visibility = "hidden";
+            element.style.opacity = "0";
+
+            if (element.hasAttribute("data-src") && element.getAttribute("data-src")) {
+                var rect = element.getBoundingClientRect();
+                if (rect.top < window.innerHeight) {
+                    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+                    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
                     var rect = element.getBoundingClientRect();
-                    if (rect.top < window.innerHeight) {
-                        const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-                        const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-                        const { top, bottom, left, right } = element.getBoundingClientRect();
-                        const isInViewport = top + element.clientHeight >= 0 &&
-                                            left + element.clientWidth >= 0 && 
-                                            bottom <= viewportHeight + element.clientHeight && 
-                                            right <= viewportWidth + element.clientWidth;
+                    var top = rect.top;
+                    var bottom = rect.bottom;
+                    var left = rect.left;
+                    var right = rect.right;
+                    var isInViewport = top + element.clientHeight >= 0 &&
+                        left + element.clientWidth >= 0 &&
+                        bottom <= viewportHeight + element.clientHeight &&
+                        right <= viewportWidth + element.clientWidth;
 
-                        if(isInViewport) {
-                            element.setAttribute("src", element.getAttribute("data-src"));
-                            element.removeAttribute("data-src");
-                            element.addEventListener('load', function () {
-                                element.previousElementSibling.remove();
-                                element.removeAttribute("style");
-                            });
-                        }
+                    if (isInViewport) {
+                        element.setAttribute("src", element.getAttribute("data-src"));
+                        element.removeAttribute("data-src");
+                        element.addEventListener('load', function () {
+                            element.previousElementSibling.remove();
+                            element.removeAttribute("style");
+                        });
                     }
                 }
-                
-            });
-        }
+            }
 
-        loadImage();
-        window.addEventListener('load', loadImage);
-        window.addEventListener('scroll', loadImage);
-        window.addEventListener('resize', loadImage);
-        window.addEventListener("wheel", loadImage);
-    });
-})( jQuery );
+        });
+    }
+
+    loadImage();
+    window.addEventListener('load', loadImage);
+    window.addEventListener('scroll', loadImage);
+    window.addEventListener('resize', loadImage);
+    window.addEventListener("wheel", loadImage);
+});
--- assets/js/svg-loader.js
+++ assets/js/svg-loader.js
@@ -1,67 +1,77 @@
 "use strict";
 
+// Wait for the DOM to be fully loaded before running the script
 document.addEventListener("DOMContentLoaded", function () {
-    svgLoader();
-});
-
-
-function svgLoader() {
     /*
     * Replace all SVG images with inline SVG
     */
-    document.querySelectorAll('img.svg').forEach(function(img) {
+   
+    // Get all image elements with class "svg"
+    var svgImg = document.querySelectorAll('img.svg');
+
+    // If no SVG images are found or their count is zero (is empty), stop the script's execution
+    if (!svgImg || svgImg.length === 0) return;
+
+    // Iterate through each SVG image
+    svgImg.forEach(function (img) {
+        // Get the URL of the SVG image
         var imgURL = img.src;
+        
         var imgAttributes = img.attributes;
         var imgAttributesObj = {};
-
-        Array.from(imgAttributes).forEach( function({name, value}) {
-            if ( name != "src" && name != "alt" ) {
-                imgAttributesObj[name] = value;
-            }
-        });
-
-        // Creating Our XMLHttpRequest object 
-        var xhr = new XMLHttpRequest();
         
+        // Collect attributes from the original image except "src" and "alt"
+        for (var i = 0; i < imgAttributes.length; i++) {
+            var attribute = imgAttributes[i];
+            var name = attribute.name;
+            var value = attribute.value;
+
+            if (name !== "src" && name !== "alt") imgAttributesObj[name] = value;
+        }
+
+        // Create an XMLHttpRequest object
+        var xhr = new XMLHttpRequest();
+
         // Making our connection
         xhr.open("GET", imgURL);
 
-        // function execute after request is successful 
+        // Define a function to execute after the request is successful 
         xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var text = this.responseText;
-                var parser = new DOMParser();
-                var xmlDoc = parser.parseFromString(text, "text/xml");
+            // Check if the request is complete and successful
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                 // Get the fetched SVG content and parse it
+                var svgContent = xhr.responseText;
 
-                // Get the SVG tag, ignore the rest
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(svgContent, "image/svg+xml");
+
+                // Get the SVG tag, ignore the rest of the content
                 var svg = xmlDoc.getElementsByTagName('svg')[0];
 
-                // Set Attributes
+                // Set attributes from the original image on the SVG
                 setAttributes(svg, imgAttributesObj);
+                
+                // Function to set attributes on an element
                 function setAttributes(element, attributes) {
-                    Object.keys(attributes).forEach(function(key) {
+                    for (var key in attributes) {
                         element.setAttribute(key, attributes[key]);
-                    });
+                    }
                 }
 
                 // Remove any invalid XML tags as per http://validator.w3.org
                 svg.removeAttribute('xmlns:a');
 
-                // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-                if(!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
-                    svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('height') + ' ' + svg.getAttribute('width'))
-                }
-                
-                // Replace image with new SVG
-                if (typeof(img.parentNode) != 'undefined' && img.parentNode != null) {
-                    img.parentNode.replaceChild(svg, img);
+                // Check if the viewBox is set; if not, set it for scaling
+                if (!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
+                    svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('width') + ' ' + svg.getAttribute('height'));
                 }
 
+                // Replace the original <img> with the new inline SVG
+                if (img.parentNode) img.parentNode.replaceChild(svg, img);
             }
-        }
+        };
 
-        // Sending our request 
+        // Send the XMLHttpRequest to fetch the SVG content
         xhr.send();
     });
-
-}
+});
